8127d7e87244c94fba39494b1e76e26cafbb6ef6
CyberWol-12 <divyau0802@gmail.com>
2026-04-23 18:16:44 +0530

    updated redmi

--- 06_top100_movies/movies.py
+++ 06_top100_movies/movies.py
@@ -1,26 +1,55 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
-movies_info = []
+product_list = []
 
-url = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+baseurl = "https://www.empireonline.com"
+url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive"
+    "User-Agent": "Mozilla/5.0"
 }
 
 r = requests.get(url, headers=headers)
-website_html = r.text
 
-soup = BeautifulSoup(website_html, "html.parser")
+soup = BeautifulSoup(r.text, "html.parser")
+songs_list = soup.find_all("div",class_ = "article-title-description__text")
+
+for item in songs_list:
+    #movies title
+    movie_tag = item.find("h3",class_ = "title").text.strip()
+    # in which year the movie released
+    year_tag = item.find("strong")
+    #link tag here we use review link and full link both
+    link_tag = item.find("a",href = True)
+    relative_link = link_tag['href']
+    full_link = baseurl + relative_link
+    #we can create 2 variable so that we can call both link independent way 
+    review_link  = relative_link
+    complete_link = full_link
+    # now we create dictionary so that we can store all items in dict
+    data_dict = {
+        "Movie":movie_tag,
+        "Year":year_tag,
+        "Review_link":review_link,
+        "full_link":complete_link
+    }
+    product_list.append(data_dict)
+
+df = pd.DataFrame(product_list)
+df.to_csv("top100_movies.csv",index = False)
+    
 
 
-title_info = soup.find_all("h3",class_ ="title")
 
-movie_title = [movie.getText() for movie in title_info]
-print(movie_title)
+
+
+
+
+
+
+
+
 
